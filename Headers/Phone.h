af8a8d3f34c80c604c4198504f2d0d9a33ce1e1d
Jcreasey23 <jcreasey23@my.whitworth.edu>
2022-12-11 22:44:28 -0800

    Initial Commit

---
+++ Headers/Phone.h
@@ -0,0 +1,45 @@
+#include <iostream>
+
+
+class Phone
+{
+private:
+    std:: string phoneNum;  // String in format (###) ### - ####
+
+    std:: string Customer_first;    // Customer First name
+    std:: string Customer_last;     // Customer Last name
+
+    // Establish type of phone?
+
+    // Establish Provider Name
+
+
+
+public:
+
+    Phone();    // Default Constructor
+
+    Phone(std:: string num, std:: string cust); // Overloaded constructor
+
+
+    // Getter Functions
+    std:: string get_PhoneNum();    // get phone number
+
+    std:: string get_Cust_f();  // get first name
+
+    std:: string get_Cust_l();  // get last name
+
+    std:: string get_Prov();    // get provider
+
+
+    // Setter Functions
+    void set_phoneNum(std:: string num);    // set number
+
+    void set_firstnm(std:: string nm);      // set first name
+
+    void set_lastnm(std:: string nm);       // set last name
+
+    void set_provider(std:: string prov);   // set provider
+
+
+};
